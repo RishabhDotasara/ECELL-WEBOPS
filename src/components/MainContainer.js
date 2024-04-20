962b196870d74f678c9c79302adda81410483107
Rishabh Dotasara <rishabhdotasara@gmail.com>
2024-04-20 10:03:27 +0530

    responsiveness added

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,29 +6,49 @@ import Mails from "./Mails";
 import Files from "./Files";
 import Todo from "./Todo";
 import { MdDateRange } from "react-icons/md";
-
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function MainContainer() {
   const [file, setFile] = useState(-1);
-  const [datetoday,setDate] = useState();
+  const [datetoday, setDate] = useState();
+  const [entact, setEntAct] = useState(false);
+  const [wrkact, setWrkAct] = useState(true);
 
-  
-  useEffect(()=>{
+  useEffect(() => {
     const today = new Date();
     setDate(`${today.getDate()}-${today.getMonth()}-${today.getFullYear()}`);
-  },[])
+  }, []);
 
   return (
     <div className="main-container">
-      <div className="ent-panel">
+      <button
+        className="ent-panel-btn btn"
+        onClick={() => {
+          setEntAct(!entact);
+        }}
+      >
+        <FaArrowRight style={{ backgroundColor: "transparent"}} />
+      </button>
+      <button
+        className="work-panel-btn btn"
+        onClick={() => {
+          setWrkAct(!wrkact);
+        }}
+      >
+        <FaArrowLeft style={{ backgroundColor: "transparent" }} />
+      </button>
+      <div className="ent-panel" style={{display: entact ? "flex" : "",left:0+"px",gap:10+"px"}}>
         {/* <div className="header">Entertainment</div> */}
         <div className="spotify-panel">
-          <SpotifyPanel/>
+          <SpotifyPanel />
         </div>
         <div className="social-media-panel">
-            <SocialMedia/>
+          <SocialMedia />
         </div>
-        <div className="mails-panel"><Mails/></div>
+        <div className="mails-panel">
+          <Mails />
+        </div>
       </div>
       <div className="working-area">
         {file !== -1 && (
@@ -38,16 +58,20 @@ export default function MainContainer() {
         )}
         {file === -1 && <span className="filler">No File Opened!</span>}
       </div>
-      <div className="work-panel">
+      <div className="work-panel" style={{display: wrkact ? "flex" : "",left:0+"px",gap:10+"px"}}>
         {/* <div className="header">Work</div> */}
-        <div className="files-panel"><Files/></div>
-        <div className="calender-panel">
-            <div className="calender-header"><MdDateRange style={{backgroundColor:"white"}}/></div>
-            <div className="date-today">
-                {datetoday}
-            </div>
+        <div className="files-panel">
+          <Files />
         </div>
-        <div className="todos-panel"><Todo/></div>
+        <div className="calender-panel">
+          <div className="calender-header">
+            <MdDateRange style={{ backgroundColor: "white" }} />
+          </div>
+          <div className="date-today">{datetoday}</div>
+        </div>
+        <div className="todos-panel">
+          <Todo />
+        </div>
       </div>
     </div>
   );
